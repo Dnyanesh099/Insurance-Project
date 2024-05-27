67e0c5954f7a8d2144cf121124d59cab811208c2
dnyanesh <dnyaneshpatil556@gmail.com>
2024-05-27 19:00:27 +0530

    Adding Entity with Database..

--- src/main/java/com/Unity/Insurance/entity/Branch.java
+++ src/main/java/com/Unity/Insurance/entity/Branch.java
@@ -1,5 +1,19 @@
 package com.Unity.Insurance.entity;
 
-public class Branch {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table
+public class Branch 
+{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String Branch_name;
+	private String Branch_city;
 }
